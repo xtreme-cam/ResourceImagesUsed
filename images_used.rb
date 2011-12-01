b146ebf3d3e082d0a5b4c1fcf726bb17ada3650b
Cam Gray & Nuo Xu <cam@xtremelabs.com>
2011-12-01 13:49:01 -0500

    CamG/Nuo - Added support for comment exclusion, verbose mode

--- images_used.rb
+++ images_used.rb
@@ -5,10 +5,13 @@ if ARGV.length < 1
   exit 1
 end
 
-unless File.directory?(ARGV[0])
+unless File.directory?(ARGV.last)
   puts "please provide a valid directory..."
   exit 1
 end
+
+verbose = ARGV.include?("-v")
+exclude_comments = ARGV.include?("-c")
 
 class Array
   def only_duplicates
@@ -34,10 +37,7 @@ class DirParser
   end
 
   def process_file(f)
-#    print "."
-    if "." == f
-      #do nothing
-    elsif ".." == f
+    if "." == f || ".." == f
       #do nothing
     elsif File.directory?(f)
       @files = Dir.entries(f).select{|i| i != "." && i != ".." }.map{ |p| "#{f}/#{p}" } + @files
@@ -62,22 +62,23 @@ class DirParser
 
 end
 
-p = DirParser.new(ARGV[0])
+p = DirParser.new(ARGV.last)
 
 def hor_line
   puts "--------------------------------------------------"
 end
 
-puts "\n"
-#hor_line
-#puts "code files:"
-#hor_line
-#puts p.code.join("\n")
-#puts "\n"
-#hor_line
-#puts "images:"
-#hor_line
-#puts p.imgs.join("\n")
+if verbose
+  hor_line
+  puts "code files:"
+  hor_line
+  puts p.code.join("\n")
+  puts "\n"
+  hor_line
+  puts "images:"
+  hor_line
+  puts p.imgs.join("\n")
+end
 
 imgs = p.imgs.map{ |i| i.split("/").last }.uniq
 not_used = imgs
@@ -88,7 +89,39 @@ p.code.each do |c|
   f = File.open(c,"r")
   contents = f.readlines
 
-  #remove comments
+  if exclude_comments
+    #remove comments
+    comment_ext = ["m","c","cpp","h","cc"]
+
+    if comment_ext.include?( c.split(".").last )
+      isInMultiLineComment = false
+      (0..(contents.length - 1)).each do |i|
+        line = contents[i]
+
+        unless isInMultiLineComment
+          matches = line.scan(/^(.*)(\/\/)(.*)$/)
+          unless matches.empty?
+            contents[i] = matches.first.first
+          end
+
+          line = contents[i]
+          matches = line.scan(/^(.*)(\/\*)(.*)$/)
+          unless matches.empty?
+            contents[i] = matches.first.first
+            isInMultiLineComment = true
+          end
+        else
+          matches = line.scan(/^(.*)(\*\/)(.*)$/)
+          if matches.empty?
+            contents[i] = ""
+          else
+            contents[i] = matches.first.last
+            isInMultiLineComment = false
+          end
+        end
+      end
+    end
+  end
 
   contents.each do |l|
     words = l.split
@@ -113,22 +146,27 @@ p.code.each do |c|
   end
 end
 
-puts "\n"
+puts "\n" if verbose
 hor_line
 puts "Resources not used:"
 hor_line
 puts not_used.join("\n")
 puts "\n"
-#hor_line
-#puts "Resources used:"
-#hor_line
-#puts usages.keys.join("\n")
+
+if verbose
+  hor_line
+  puts "Resources used:"
+  hor_line
+  puts usages.keys.join("\n")
+end
 
 
-dup_imgs = p.imgs.map{ |i| i.split("/").last }.only_duplicates.uniq
-puts "\n"
-hor_line
-puts "Duplicate Resources:"
-hor_line
-puts dup_imgs.join("\n")
-puts "\n"
+if verbose
+  dup_imgs = p.imgs.map{ |i| i.split("/").last }.only_duplicates.uniq
+  puts "\n"
+  hor_line
+  puts "Duplicate Resources:"
+  hor_line
+  puts dup_imgs.join("\n")
+  puts "\n"
+end
